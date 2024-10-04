3c9c9be7d44fe024832c9f20e292221f86204b63
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-04 15:31:27 -0300

    Implementando singleton

--- lab-padroesprojeto/src/one/digitalinovation/gof/Test.java
+++ lab-padroesprojeto/src/one/digitalinovation/gof/Test.java
@@ -1,5 +1,10 @@
 package one.digitalinovation.gof;
 
+import one.digitalinovation.gof.singleton.SingletonEager;
+import one.digitalinovation.gof.singleton.SingletonLazy;
+import one.digitalinovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinovation.gof.strategy.*;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -19,6 +24,23 @@ public class Test {
         lazyHolder = SingletonLazyHolder.getInstance();
         System.out.println(lazyHolder);
 
+        //Strategy
+
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+
+        robo.setComportamento(defensivo);
+        robo.mover();
+
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
     }
 
 
