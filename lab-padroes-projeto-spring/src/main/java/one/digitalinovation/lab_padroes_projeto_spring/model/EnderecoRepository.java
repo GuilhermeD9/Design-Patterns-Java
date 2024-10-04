4bc38d31ba885194aee406899bb364806821726f
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-04 16:07:00 -0300

    insert: model classes

---
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinovation/lab_padroes_projeto_spring/model/EnderecoRepository.java
@@ -0,0 +1,6 @@
+package one.digitalinovation.lab_padroes_projeto_spring.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository<Endereco, String> {
+}
