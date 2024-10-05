5767e096d943298b6af213577d7444722dc05ac4
Guilherme Silva <sgsouza.173@gmail.com>
2024-10-05 11:33:52 -0300

    create: ClienteServiceImpl

--- lab-padroes-projeto-spring/src/main/java/one/digitalinovation/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplication.java
+++ lab-padroes-projeto-spring/src/main/java/one/digitalinovation/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplication.java
@@ -2,6 +2,7 @@ package one.digitalinovation.lab_padroes_projeto_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
